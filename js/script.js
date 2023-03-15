82dd023807317b41777a4d55a9f02902ecb53090
stocaline <stocaline2@gmail.com>
2023-03-15 14:35:02 -0300

    Adicionando menu slider com mais personagens

--- js/script.js
+++ js/script.js
@@ -25,6 +25,15 @@ const agents = [
     "e370fa57-4757-3604-3648-499e1f642d3f", //Gekko
     "f94c3b30-42be-e959-889c-5aa313dba261", //Raze
     "a3bfb853-43b2-7238-a4f1-ad90e9e46bcc", //Reyna
+    "601dbbe7-43ce-be57-2a40-4abd24953621", //Kay/o
+    "6f2a04ca-43e0-be17-7f36-b3908627744d", //Skye
+    "117ed9e3-49f3-6512-3ccf-0cada7e3823b", //Cypher
+    "320b2a48-4d9b-a075-30f1-1f93a9b638fa", //Sova
+    "1e58de9c-4950-5125-93e9-a0aee9f98746", //KillJoy
+    "707eab51-4836-f488-046a-cda6bf494859", //Viper
+    "eb93336a-449b-9c1b-0a54-a891f7921d69", //Phoenix
+    "9f0d8ba9-4140-b941-57d3-a7ad57c6b417", //Brimstone
+    "bb2a4828-46eb-8cd1-e765-15848195d751", //Neon
 
 ]
 
@@ -65,3 +74,23 @@ selectAgent.forEach(button => {
         document.body.style.background = `linear-gradient(-110deg, #${agentData.data.backgroundGradientColors.join(", #")})`
     })
 })
+
+
+
+
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+let currentPosition = 0;
+
+function moveSlider(direction) {
+  if (direction === 'prev' && currentPosition > 0) {
+    currentPosition--;
+  } else if (direction === 'next' && currentPosition < slider.children.length - 1) {
+    currentPosition++;
+  }
+  slider.style.transform = `translateX(-${currentPosition * 20}rem)`;
+}
+
+prevButton.addEventListener('click', () => moveSlider('prev'));
+nextButton.addEventListener('click', () => moveSlider('next'));
